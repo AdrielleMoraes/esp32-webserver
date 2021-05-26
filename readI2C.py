e68e07096daa50d9f939a284d4f1585cc8081fb6
Adrielle <adrielle.nazar@hotmail.com>
2021-05-26 13:57:52 +0100

    new file to read i2c values from board

---
+++ readI2C.py
@@ -0,0 +1,9 @@
+# function to collect data via the I2c protocol
+
+def readtemp(scl_pin=5, sda_pin=4):
+    import machine
+    i2c = machine.I2C(scl=machine.Pin(scl_pin), sda=machine.Pin(sda_pin))
+    byte_data = bytearray(2)
+    i2c.readfrom_mem_into(24, 5, byte_data)
+    value = byte_data[0] << 8 | byte_data[1]
+    return (value & 0xFFF) / 16.0
--- wifitools.py
+++ wifitools.py
@@ -1,2 +1,36 @@
 """ This module contains a couple of helper functions that allow the ESP8266 to connect to a WiFi network and make GET requests. """
 
+import urequests
+
+#https://docs.micropython.org/en/v1.8.6/esp8266/esp8266/tutorial/network_basics.html
+def connect(SSID,password):
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, password)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+def getmac():
+    import network
+    import ubinascii
+    return ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+
+def flaskiot_post(API_key,mac_address,field, data):
+    if not isinstance(data, str):
+        data = str(data)
+    if not isinstance(field, str):
+        field = str(field)
+    # https://freetemp.org/update/API_key=ASCIISTR/mac=6c:rf:7f:2b:0e:g8/field=1/data=72.3
+    base_url = 'https://freetemp.org/update'
+    api_key_url = '/API_key=' + API_key
+    mac_url = '/mac=' + mac_address
+    field_url = '/field=' + field
+    data_url = '/data=' + str(data)
+    url = base_url + api_key_url + mac_url + field_url + data_url
+    print(url)
+    response = urequests.get(url)
+    print(response.text)
