3c7f5442b551fe87f47543805ac3498782840f15
Adrielle <adrielle.nazar@hotmail.com>
2021-05-27 23:32:05 +0100

    remove main

--- boot.py
+++ boot.py
@@ -1,1 +1,28 @@
 # This file will first run 
+import config 
+import run
+
+import network
+
+import gc
+gc.collect()
+
+ssid = config.SSID
+password = config.WIFI_PASSWORD
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+
+print("Starting Server...")
+run.main()
+
+
--- run.py
+++ run.py
@@ -1,30 +1,67 @@
-"""
-connect to the WiFi network
-read the temperature off of the MCP9808 temperature sensor
-try to post the temperature to our flask IoT server
-wait 1 minute
-"""
-
-import wifitools
-import readI2C
 import time
-import config
-import random
+from hcsr04 import HCSR04
+import homePage
+from machine import Pin
+import gc
+
+try:
+  import usocket as socket
+except:
+  import socket
 
 def main():
-    ssid = config.SSID
-    password = config.WIFI_PASSWORD
-    api_key = config.API_KEY
-    mac_address = config.MAC_ADDRESS
-    field = '1'
+    
+    # set pins
+    led = Pin(12, Pin.OUT)
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    # create web socket
 
-    wifitools.connect(ssid,password)
-    time.sleep(5)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 80))
+    s.listen(5)
 
-    for i in range(60*8):
-        data = random.randint(0, 100)#readI2C.readValue()
+    # distance sensor data
+    sensor = HCSR04(trigger_pin=27, echo_pin=26,echo_timeout_us=1000000)
+
+    while True:
+
+        # read data
+        distance = sensor.distance_cm()
+
         try:
-            wifitools.flaskiot_post(api_key,mac_address,field,data)
-        except:
-            pass
-        time.sleep(60)
+            if gc.mem_free() < 102000:
+                gc.collect()
+            conn, addr = s.accept()
+            conn.settimeout(3.0)
+            print('Received HTTP GET connection request from %s' % str(addr))
+            request = conn.recv(1024)
+            conn.settimeout(None)
+            request = str(request)
+            print('GET Rquest Content = %s' % request)
+            led_on = request.find('/?led_2_on')
+            led_off = request.find('/?led_2_off')
+
+            if led_on == 6:
+                print('LED ON -> GPIO2')
+                led_state = "ON"
+                led.value(1)
+
+            if led_off == 6:
+                print('LED OFF -> GPIO2')
+                led_state = "OFF"
+                led.value(0)
+
+            response = homePage.web_page(data=str(distance))
+
+            conn.send('HTTP/1.1 200 OK\n')
+            conn.send('Content-Type: text/html\n')
+            conn.send('Connection: close\n\n')
+            conn.sendall(response)
+            conn.close()
+
+        except OSError as e:
+            s.close()
+            conn.close()
+            print('Connection closed')
