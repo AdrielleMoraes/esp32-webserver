bd6ca92d7a13466568957bd7a249be2d6c421b5f
Adrielle <adrielle.nazar@hotmail.com>
2021-06-07 23:41:43 +0100

    New db

--- server/polls/models.py
+++ server/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.TextField()
+    def __str__(self):
+        return self.name
+
+class Esp32(models.Model):
+
+    distance = models.IntegerField()
+    led = models.BooleanField()
+    owner = models.ForeignKey("User", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "Status: " + self.led
