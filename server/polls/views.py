37db650362e1ff7a84493d80fecc14032badd466
Adrielle <adrielle.nazar@hotmail.com>
2021-06-09 22:35:35 +0100

    creating API

--- server/polls/views.py
+++ server/polls/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import User
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    user_list = User.objects.all()
+    print(user_list)
+    output = ', '.join([user.name for user in user_list])
+    context = {
+        'user_list': user_list,
+    }
+
+    return render(request, 'index.html', context)
