a19dbc3da4989f6eec43b7f7ca866cb4730ce4b2
Adrielle <adrielle.nazar@hotmail.com>
2021-06-19 12:59:00 +0100

    feature to auto connect ESP32 to wifi

--- Esp32/boot.py
+++ Esp32/boot.py
@@ -1,26 +1,44 @@
 # This file will first run 
-import config 
+# import config 
+# import run
+
+# import network
+
+# import gc
+# gc.collect()
+
+# # implement mqtt instead with autoconnect
+# ssid = config.SSID
+# password = config.WIFI_PASSWORD
+
+# station = network.WLAN(network.STA_IF)
+
+# station.active(True)
+# station.connect(ssid, password)
+
+# while station.isconnected() == False:
+#   pass
+
+# print('Connection successful')
+# print(station.ifconfig())
+
+import wifimgr # wifi manager to handle wifi connectivity
+from time import sleep
 import run
 
-import network
+try:
+  import usocket as socket
+except:
+  import socket
 
-import gc
-gc.collect()
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
 
-ssid = config.SSID
-password = config.WIFI_PASSWORD
-
-station = network.WLAN(network.STA_IF)
-
-station.active(True)
-station.connect(ssid, password)
-
-while station.isconnected() == False:
-  pass
-
-print('Connection successful')
-print(station.ifconfig())
-
+# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
+print("ESP OK")
 
 print("Starting Server...")
 run.main()
