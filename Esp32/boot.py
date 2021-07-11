aad11c60f6fb45c7184c30a27ee79fd3962c4a6d
Adrielle <adrielle.nazar@hotmail.com>
2021-07-11 13:37:17 +0100

    updating wifi access

--- Esp32/boot.py
+++ Esp32/boot.py
@@ -1,27 +1,3 @@
-# This file will first run 
-# import config 
-# import run
-
-# import network
-
-# import gc
-# gc.collect()
-
-# # implement mqtt instead with autoconnect
-# ssid = config.SSID
-# password = config.WIFI_PASSWORD
-
-# station = network.WLAN(network.STA_IF)
-
-# station.active(True)
-# station.connect(ssid, password)
-
-# while station.isconnected() == False:
-#   pass
-
-# print('Connection successful')
-# print(station.ifconfig())
-
 import wifimgr # wifi manager to handle wifi connectivity
 from time import sleep
 import run
