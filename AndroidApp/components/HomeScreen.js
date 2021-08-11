15aea6c7875d9d806715262a9d56abe043866e84
Adrielle <adrielle.nazar@hotmail.com>
2021-08-11 20:07:08 +0100

    profile screen

--- AndroidApp/components/HomeScreen.js
+++ AndroidApp/components/HomeScreen.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import {Button} from 'react-native';
+import React from 'react'
+import {Button} from 'react-native'
 
-const HomeScreen = ({ navigation }) =>{
-    return (
-      <Button
-        title="Go to your profile"
-        onPress={() =>
-          navigation.navigate('Profile', { name: 'Adrielle' })
-        }
-      />
-    );
-  };
-
-export default HomeScreen;
+const HomeScreen = ({navigation}) => {
+  return (
+    <Button
+      title="Login"
+      onPress={() => navigation.navigate('Profile', {name: 'Adrielle'})}
+    />
+  )
+}
+export default HomeScreen
--- AndroidApp/components/ProfileScreen.js
+++ AndroidApp/components/ProfileScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text} from 'react-native';
+import {Avatar} from 'react-native-elements';
 
-const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
+const ProfileScreen = ({navigation, route}) => {
+  return (
+    <View>
+      <Avatar
+        rounded
+        size="medium"
+        title="AM"
+        activeOpacity={0.7}
+        overlayContainerStyle={{backgroundColor: 'green'}}
+      />
+      <Text>This is {route.params.name}'s profile</Text>
+    </View>
+  );
 };
 
 export default ProfileScreen;
