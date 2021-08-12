722b63284a06ef299e448e8a8f5be69d76469adb
Adrielle <adrielle.nazar@hotmail.com>
2021-08-12 18:06:10 +0100

    adding styles

--- AndroidApp/components/ProfileScreen.js
+++ AndroidApp/components/ProfileScreen.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {Avatar} from 'react-native-elements';
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    textAlign: "center",
+    marginHorizontal: 16,
+    marginTop: 12,
+  },
+  blueText: {
+    color: "blue",
+  },
+  greyText: {
+    color: "grey",
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    backgroundColor: 'gray',
+  },
+});
+
 
 const ProfileScreen = ({navigation, route}) => {
   return (
@@ -13,6 +34,9 @@ const ProfileScreen = ({navigation, route}) => {
         overlayContainerStyle={{backgroundColor: 'green'}}
       />
       <Text>This is {route.params.name}'s profile</Text>
+      <Text>Connect to Sensor</Text>
+      <Text>Conected sensors</Text>
+      <View style={styles.image}></View>
     </View>
   );
 };
