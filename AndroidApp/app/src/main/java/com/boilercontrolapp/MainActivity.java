ee94b0fcd4d953eb349438b2d4fb05f37200cf1d
Adrielle <adrielle.nazar@hotmail.com>
2022-02-23 21:10:05 +0000

    login page

--- AndroidApp/app/src/main/java/com/boilercontrolapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/boilercontrolapp/MainActivity.java
@@ -17,17 +17,7 @@ public class MainActivity extends AppCompatActivity {
     //method called when button is pressed
     public void SendMessage(View view){
         //Do something in response to the button
-        TextView txtFirstName = findViewById(R.id.textFirstName);
-        TextView txtLastName = findViewById(R.id.textLastName);
-        TextView txtEmail = findViewById(R.id.textEmail);
 
 
-        EditText editFirstName = findViewById(R.id.editFirstName);
-        EditText editLastName = findViewById(R.id.editLastName);
-        EditText editEmail = findViewById(R.id.editEmail);
-
-        txtFirstName.setText(editFirstName.getText());
-        txtLastName.setText(editLastName.getText());
-        txtEmail.setText(editEmail.getText());
     }
 }
