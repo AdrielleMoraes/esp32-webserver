8a8d1d4bc1ff2f5998a605bf33dc95cf572a8aba
Adrielle <adrielle.nazar@hotmail.com>
2022-04-13 23:28:21 +0100

    interface changes

--- AndroidApp/app/src/main/java/com/boilercontrolapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/boilercontrolapp/MainActivity.java
@@ -3,15 +3,29 @@ package com.boilercontrolapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btnLogin:
+                Toast.makeText(this, "Hello Button", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginBtn = findViewById(R.id.btnLogin);
+
+        loginBtn.setOnClickListener(this);
     }
 
     //method called when button is pressed
